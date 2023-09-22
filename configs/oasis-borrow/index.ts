53d172a1e8a56f6a8b01812614b90feb1e75074e
sebastianPiekarczyk <sebastian@oazoapps.com>
2023-09-22 10:44:59 +0200

    Extend config with navigation specific data

--- configs/oasis-borrow/index.ts
+++ configs/oasis-borrow/index.ts
@@ -42,15 +42,139 @@ export default function ({
     },
     parameters: {
       topBanner: {
-        name: "rebranding",
-        url: "https://blog.summer.fi/oasis-app-rebrands-to-summer-fi/",
-        message: "Oasis.app is now Summer.fi! Read the announcement",
+        name: 'rebranding',
+        url: 'https://blog.summer.fi/oasis-app-rebrands-to-summer-fi/',
+        message: 'Oasis.app is now Summer.fi! Read the announcement',
       },
       aaveLike: {
         orderInformation: {
           showFlashloanInformation: notProduction,
         },
       },
+      navigation: {
+        protocols: {
+          ajna: {
+            borrow: {
+              tokens: ['ETH', 'WBTC', 'DAI'],
+              extra: {
+                title: 'Amplify sDAI up to 35x.',
+                description:
+                    'Use Summer.fi multiply to increase your exposure to sDAI yield seamlessly.',
+                url: '/',
+              },
+            },
+            multiply: {
+              tokens: ['ETH', 'WBTC', 'DAI'],
+              extra: {
+                title: 'Amplify sDAI up to 35x.',
+                description:
+                    'Use Summer.fi multiply to increase your exposure to sDAI yield seamlessly.',
+                url: '/',
+              },
+            },
+            earn: {
+              tokens: ['ETH', 'WBTC', 'DAI'],
+              extra: {
+                title: 'Amplify sDAI up to 35x.',
+                description:
+                    'Use Summer.fi multiply to increase your exposure to sDAI yield seamlessly.',
+                url: '/',
+              },
+            },
+          },
+          aave: {
+            borrow: {
+              tokens: ['ETH', 'WBTC', 'DAI'],
+              extra: {
+                title: 'Amplify sDAI up to 35x.',
+                description:
+                    'Use Summer.fi multiply to increase your exposure to sDAI yield seamlessly.',
+                url: '/',
+              },
+            },
+            multiply: {
+              tokens: ['ETH', 'WBTC', 'DAI'],
+              extra: {
+                title: 'Amplify sDAI up to 35x.',
+                description:
+                    'Use Summer.fi multiply to increase your exposure to sDAI yield seamlessly.',
+                url: '',
+              },
+            },
+            earn: {
+              tokens: ['ETH', 'WBTC', 'DAI'],
+              extra: {
+                title: 'Amplify sDAI up to 35x.',
+                description:
+                    'Use Summer.fi multiply to increase your exposure to sDAI yield seamlessly.',
+                url: '/',
+              },
+            },
+          },
+          maker: {
+            borrow: {
+              tokens: ['ETH', 'WBTC', 'DAI'],
+              extra: {
+                title: 'Amplify sDAI up to 35x.',
+                description:
+                    'Use Summer.fi multiply to increase your exposure to sDAI yield seamlessly.',
+                url: '',
+              },
+            },
+            multiply: {
+              tokens: ['ETH', 'WBTC', 'DAI'],
+              extra: {
+                title: 'Amplify sDAI up to 35x.',
+                description:
+                    'Use Summer.fi multiply to increase your exposure to sDAI yield seamlessly.',
+                url: '',
+              },
+            },
+            earn: {
+              tokens: ['ETH', 'WBTC', 'DAI'],
+              extra: {
+                title: 'Amplify sDAI up to 35x.',
+                description:
+                    'Use Summer.fi multiply to increase your exposure to sDAI yield seamlessly.',
+                url: '',
+              },
+            },
+          },
+          spark: {
+            borrow: {
+              tokens: ['ETH', 'WBTC', 'DAI'],
+              extra: {
+                title: 'Amplify sDAI up to 35x.',
+                description:
+                    'Use Summer.fi multiply to increase your exposure to sDAI yield seamlessly.',
+                url: '',
+              },
+            },
+            multiply: {
+              tokens: ['ETH', 'WBTC', 'DAI'],
+              extra: {
+                title: 'Amplify sDAI up to 35x.',
+                description:
+                    'Use Summer.fi multiply to increase your exposure to sDAI yield seamlessly.',
+                url: '',
+              },
+            },
+            earn: {
+              tokens: ['ETH', 'WBTC', 'DAI'],
+              extra: {
+                title: 'Amplify sDAI up to 35x.',
+                description:
+                    'Use Summer.fi multiply to increase your exposure to sDAI yield seamlessly.',
+                url: '',
+              },
+            },
+          },
+        },
+        tokens: {
+          popular: ['ETH, STETH', 'RETH', 'CBETH', 'DAI'],
+          new: ['SDAI', 'GHO', 'TBTC'],
+        },
+      },
     },
-  };
+  }
 }
