209f787866e69124cf26a4ab4aa1188cd92f49c8
halaprix <panicz.klos@gmail.com>
2023-09-27 14:59:06 +0200

    feat: add stage and source to rpcConfig

--- configs/oasis-borrow/getRpcConfig.ts
+++ configs/oasis-borrow/getRpcConfig.ts
@@ -1,5 +1,11 @@
-export const getRpcConfig = () => ({
+import { ConfigHelperType } from "⌨️";
+
+type PickConfigHelper = Pick<ConfigHelperType, "notProduction">;
+
+export const getRpcConfig = ({ notProduction }: PickConfigHelper) => ({
   skipCache: false,
   skipMulticall: false,
   skipGraph: true,
+  stage: notProduction ? "staging" : "prod",
+  source: notProduction ? "borrow-staging" : "borrow-prod",
 });
