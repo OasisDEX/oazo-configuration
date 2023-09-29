c5613a444f6edfa1fa5609a1b246d539d5283d4a
piekczyk <piekarczyk.sebastian@gmail.com>
2023-09-29 10:08:10 +0200

    Update navigation protocol items (#45)

--- configs/oasis-borrow/getNavigation.ts
+++ configs/oasis-borrow/getNavigation.ts
@@ -2,71 +2,70 @@ export const getNavigation= () => ({
     protocols: {
         ajna: {
             borrow: {
-                tokens: ['ETH', 'WBTC', 'DAI'],
+                description: 'Borrow against ETH, WBTC, DAI and any other type of collateral with AJNA.',
             },
             multiply: {
-                tokens: ['ETH', 'WBTC', 'DAI'],
+                description: 'Increase exposure to ETH, WBTC and more with AJNA; access top risk management tools.',
             },
             earn: {
-                tokens: ['ETH', 'WBTC', 'DAI'],
-
+                description: 'Earn yield on ETH, WBTC, DAI and more with AJNA; and get exclusive token rewards.',
             },
             extra: {
                 title: 'Amplify sDAI up to 35x.',
                 description:
                     'Use Summer.fi multiply to increase your exposure to sDAI yield seamlessly.',
-                url: '/',
+                url: '/ethereum/ajna/multiply/SDAI-USDC',
             },
         },
         aave: {
             borrow: {
-                tokens: ['ETH', 'WBTC', 'DAI'],
+                description: 'Borrow against ETH, WBTC, sDAI and other types of collateral.',
             },
             multiply: {
-                tokens: ['ETH', 'WBTC', 'DAI'],
+                description: 'Increase exposure to ETH, WBTC and more; access top risk management tools.',
             },
             earn: {
-                tokens: ['ETH', 'WBTC', 'DAI'],
+                description: 'Earn yield on ETH, sDAI and more; lend or yield loop strategies in one transaction.',
             },
             extra: {
-                title: 'Amplify sDAI up to 35x.',
+                title: 'Amplify stETH up to 9.99x.',
                 description:
-                    'Use Summer.fi multiply to increase your exposure to sDAI yield seamlessly.',
-                url: '/',
+                    'Use Summer.fi earn to increase your exposure to stETH yield seamlessly.',
+                url: '/ethereum/aave/v3/earn/wstetheth',
             },
         },
         maker: {
             borrow: {
-                tokens: ['ETH', 'WBTC', 'DAI'],
+                description: 'Borrow DAI against ETH, LSTs and WBTC.',
             },
             multiply: {
-                tokens: ['ETH', 'WBTC', 'DAI'],
+                description: 'Increase exposure to ETH, LSTs and WBTC; access top risk management tools.',
             },
             earn: {
-                tokens: ['ETH', 'WBTC', 'DAI'],
+                description: 'Earn with the Dai Savings Rate and mint sDAI.',
             },
             extra: {
-                title: 'Amplify sDAI up to 35x.',
+                title: 'Access the DSR',
                 description:
-                    'Use Summer.fi multiply to increase your exposure to sDAI yield seamlessly.',
-                url: '/',
+                    'Earn risk reduced yield on DAI with the Maker Protocols Dai Savings Rate.',
+                url: '/earn/dsr',
             },
         },
         spark: {
             borrow: {
-                tokens: ['ETH', 'WBTC', 'DAI'],
+                description: 'Borrow DAI at the best rate, with the highest LTVs. Become eligible for token rewards.',
             },
             multiply: {
-                tokens: ['ETH', 'WBTC', 'DAI'],
+                description: 'Increase exposure to ETH and LSTs; access top risk management tools.',
             },
             earn: {
-                tokens: ['ETH', 'WBTC', 'DAI'],
+                description: 'Earn yield on ETH and DAI. Become eligible for token rewards.',
             },
             extra: {
-                title: 'Amplify sDAI up to 35x.',
+                title: 'Amplify stETH up to 9.99x.',
                 description:
-                    'Use Summer.fi multiply to increase your exposure to sDAI yield seamlessly.',
-                url: '/',
+                    'Use Summer.fi earn to increase your exposure to stETH yield seamlessly.',
+                url: '/ethereum/spark/v3/earn/wstetheth',
             },
         },
     },
