28fb9b517341cb7547eeb57215b4026d43d9989b
Marcin Ciarka <marcin@oazoapps.com>
2023-10-03 14:22:27 +0300

    'closeDisabledFor' collateral/strategytypes in aave like (#46)

--- configs/oasis-borrow/getParameters.ts
+++ configs/oasis-borrow/getParameters.ts
@@ -1,14 +1,20 @@
-import {ConfigHelperType} from "⌨️";
+import { ConfigHelperType } from "⌨️";
 
-export const getParameters = ({notProduction}: Pick<ConfigHelperType, 'notProduction'>) => ({
-    topBanner: {
-        name: 'rebranding',
-        url: 'https://blog.summer.fi/oasis-app-rebrands-to-summer-fi/',
-        message: 'Oasis.app is now Summer.fi! Read the announcement',
+export const getParameters = ({
+  notProduction,
+}: Pick<ConfigHelperType, "notProduction">) => ({
+  topBanner: {
+    name: "rebranding",
+    url: "https://blog.summer.fi/oasis-app-rebrands-to-summer-fi/",
+    message: "Oasis.app is now Summer.fi! Read the announcement",
+  },
+  aaveLike: {
+    orderInformation: {
+      showFlashloanInformation: notProduction,
     },
-    aaveLike: {
-        orderInformation: {
-            showFlashloanInformation: notProduction,
-        },
+    closeDisabledFor: {
+      collateral: ["DAI", "USDC"],
+      strategyTypes: ["short"],
     },
-})
+  },
+});
