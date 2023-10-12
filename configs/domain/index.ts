7ebfb0dd20031fcb1fc045458119201e7801fd26
James Tuckett <james@tuckett.me>
2023-10-12 09:21:39 +0100

    feat: create getTokens function

--- configs/domain/index.ts
+++ configs/domain/index.ts
@@ -4,15 +4,21 @@ import {ConfigHelperType} from "⌨️";
 // import { getParameters } from "./getParameters";
 // import { getRpcConfig } from "./getRpcConfig";
 
+
 export default function ({
                              isDevelopment: _isDevelopment,
                              notProduction,
                              isStaging,
                          }: ConfigHelperType) {
+    /**
+     * Goal is to capture domain specific configurations that is common
+     * across all Summer.fi systems and apps.
+     *
+     * Possible extensions could include Products / Product categories / Strategies etc
+     */
     return {
-        // features: getFeatures({ isStaging }),
-        // parameters: getParameters({ notProduction }),
-        // navigation: getNavigation(),
-        // rpcConfig: getRpcConfig({ notProduction }),
+        tokens: null,
+        lendingProtocols: null,
+        networks: null,
     };
 }
