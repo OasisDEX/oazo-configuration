9e790642bae0198fb70c4064f08ed18c122edfe8
James Tuckett <james@tuckett.me>
2023-10-12 09:12:44 +0100

    feat: create token config

---
+++ configs/token/index.ts
@@ -0,0 +1,18 @@
+import {ConfigHelperType} from "⌨️";
+// import { getFeatures } from "./getFeatures";
+// import { getNavigation } from "./getNavigation";
+// import { getParameters } from "./getParameters";
+// import { getRpcConfig } from "./getRpcConfig";
+
+export default function ({
+                             isDevelopment: _isDevelopment,
+                             notProduction,
+                             isStaging,
+                         }: ConfigHelperType) {
+    return {
+        // features: getFeatures({ isStaging }),
+        // parameters: getParameters({ notProduction }),
+        // navigation: getNavigation(),
+        // rpcConfig: getRpcConfig({ notProduction }),
+    };
+}
