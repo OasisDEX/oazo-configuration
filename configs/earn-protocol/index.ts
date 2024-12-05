e7e22b1eff5f603664f4800757729d71dff279cc
Marcin Ciarka <marcin@oazoapps.com>
2024-12-05 12:16:33 +0100

    feat: add earn protocol configuration and network identifiers

---
+++ configs/earn-protocol/index.ts
@@ -0,0 +1,10 @@
+import { ConfigHelperType } from "⌨️";
+import { getFleetConfig } from "./getFleetConfig";
+import { getLinks } from "./getLinks";
+
+export default function (params: ConfigHelperType) {
+  return {
+    links: getLinks(params),
+    fleetMap: getFleetConfig(),
+  };
+}
