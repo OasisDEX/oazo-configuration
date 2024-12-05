38c1765d57423dc7a84cc9ea6cfc4ebe8407038c
Marcin Ciarka <marcin@oazoapps.com>
2024-12-05 16:28:27 +0100

    feat: enhance getFleetConfig with address fields for type safety and specific network identifiers

--- configs/earn-protocol/getFleetConfig.ts
+++ configs/earn-protocol/getFleetConfig.ts
@@ -2,6 +2,7 @@ import { NetworkIds, Risk } from "🤝";
 
 type FleetConfig = {
   [contractAddress: `0x${string}`]: {
+    address: `0x${string}` | "";
     name: string;
     slug: string;
     bestFor: string;
@@ -17,6 +18,7 @@ const emptyConfig: FleetConfig = {
   // just for the (later) type safety - so everything has the same
   // type and is defined dont actually use "0x" key please
   "0x": {
+    address: "",
     name: "",
     slug: "",
     bestFor: "",
@@ -31,6 +33,7 @@ export const getFleetConfig: GetFleetConfig = () => ({
   [NetworkIds.ARBITRUMMAINNET]: {
     ...emptyConfig,
     "0x2653014cd3ad332a98b0a80ccf12473740df81c2": {
+      address: "0x2653014cd3ad332a98b0a80ccf12473740df81c2",
       name: "Earn McYieldFace USDC",
       slug: "earn-mcyieldface-usdc",
       bestFor: "Chill earning",
@@ -40,6 +43,7 @@ export const getFleetConfig: GetFleetConfig = () => ({
   [NetworkIds.BASEMAINNET]: {
     ...emptyConfig,
     "0xeb201f4915b6cbff5a01abd866fe6c6a026f224d": {
+      address: "0xeb201f4915b6cbff5a01abd866fe6c6a026f224d",
       name: "USDC Ya Later",
       slug: "usdc-ya-later",
       bestFor: "Earning while asleep",
