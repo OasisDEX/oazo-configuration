ab1d68cb6f9b6592a573a907c88cf65023fefd61
Marcin Ciarka <marcin@oazoapps.com>
2025-05-08 21:20:31 +0200

    fix: update VaultSwitching logic to enable on staging and development environments

--- configs/earn-protocol/getFeatures.ts
+++ configs/earn-protocol/getFeatures.ts
@@ -2,11 +2,11 @@ import { ConfigHelperType } from "⌨️";
 
 export const getFeatures = ({
   isStaging: _isStaging,
-  isDevelopment,
+  isDevelopment: _isDevelopment,
   isProduction,
 }: ConfigHelperType) => ({
   Migrations: true,
   Send: true,
   FilterZeroTokenVaults: isProduction, // filter out zero token vaults on production
-  VaultSwitching: isDevelopment, // enable vault switching in development
+  VaultSwitching: !isProduction, // enable vault switching on staging and development
 });
