73f04b19c226394995328daf97cd17cf65f9f5a5
piekczyk <sebastian@oazoapps.com>
2025-05-22 09:02:27 +0200

    refactor: simplify getFeatures parameters and update BeachClub logic

--- configs/earn-protocol/getFeatures.ts
+++ configs/earn-protocol/getFeatures.ts
@@ -1,12 +1,13 @@
 import { ConfigHelperType } from "⌨️";
 
 export const getFeatures = ({
-  isStaging: _isStaging,
-  isDevelopment: _isDevelopment,
+  isStaging,
+  isDevelopment,
   isProduction,
 }: ConfigHelperType) => ({
   Migrations: true,
   Send: true,
   FilterZeroTokenVaults: isProduction, // filter out zero token vaults on production
   VaultSwitching: true,
+  BeachClub: isStaging || isDevelopment,
 });
