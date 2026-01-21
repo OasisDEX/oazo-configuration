8a25f1834d3dfaa6601ed17d3cd183c4f925fa8c
Marcin Ciarka <marcin@oazoapps.com>
2026-01-22 00:02:09 +0100

    Update SumrCoingeckoTokenID to use a fixed value for production environment

--- configs/earn-protocol/getFeatures.ts
+++ configs/earn-protocol/getFeatures.ts
@@ -13,5 +13,5 @@ export const getFeatures = ({ isProduction }: ConfigHelperType) => ({
   Game: true,
   StakingV2: true,
   UseSumrCoingeckoPrice: !isProduction,
-  SumrCoingeckoTokenID: isProduction ? "summer-2" : "stp-network", // checking if everything works fine with other token on dev
+  SumrCoingeckoTokenID: "stp-network", // checking if everything works fine with other token on prod
 });
