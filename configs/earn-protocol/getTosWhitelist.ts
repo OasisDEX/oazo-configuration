73b200ce01773b925ddacf1d2a194bd6106c26d7
Marcin Ciarka <marcin@oazoapps.com>
2026-02-26 17:24:47 +0100

    Add additional address to getTosWhitelist and improve formatting

--- configs/earn-protocol/getTosWhitelist.ts
+++ configs/earn-protocol/getTosWhitelist.ts
@@ -1,4 +1,6 @@
-export const getTosWhitelist = () => ([
-  '0xCe6ca762a8c2dd1483ec7dF714CEA751d03260b2',
-  '0xdb6e9e7390e9acc34619e56efa48ade01cff6f12',
-].map(address => address.toLowerCase()));
+export const getTosWhitelist = () =>
+  [
+    "0xCe6ca762a8c2dd1483ec7dF714CEA751d03260b2",
+    "0xdb6e9e7390e9acc34619e56efa48ade01cff6f12",
+    "0x1aF8aA70Cd2FdC8b0c97d632DE8dc8894E043b47",
+  ].map((address) => address.toLowerCase());
