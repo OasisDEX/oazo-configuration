b05c656234083c8eecbfb0f2fe4b1a791c12b36a
Marcin Ciarka <marcin@oazoapps.com>
2026-03-11 18:04:55 +0100

    Add new address to getTosWhitelist

--- configs/earn-protocol/getTosWhitelist.ts
+++ configs/earn-protocol/getTosWhitelist.ts
@@ -3,4 +3,5 @@ export const getTosWhitelist = () =>
     "0xCe6ca762a8c2dd1483ec7dF714CEA751d03260b2",
     "0xdb6e9e7390e9acc34619e56efa48ade01cff6f12",
     "0x1aF8aA70Cd2FdC8b0c97d632DE8dc8894E043b47",
+    "0x1Ec7A44C634e226Ab23Bc635B0628c1430b764d3",
   ].map((address) => address.toLowerCase());
